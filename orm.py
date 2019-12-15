a195bac0553af617b83fab8edaf256e9173912f2
SLB974 <slb974run@gmail.com>
2019-12-15 05:57:10 +0400

    Updating orm.py Define engine, session, Base and metadata. Only declarative classes

--- orm.py
+++ orm.py
@@ -1,18 +1,17 @@
 # coding: utf-8
 import constants as ct
-import API_scripts as api
 from sqlalchemy import CHAR, Column, DateTime, ForeignKey, String
 from sqlalchemy.dialects.mysql import INTEGER
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import inspect
+from sqlalchemy import create_engine
 
 """
 Manage ORM
 """
-
+engine = create_engine(ct.connection_source)
+session = sessionmaker(bind=engine)
 Base = declarative_base()
 metadata = Base.metadata
 
@@ -22,6 +21,10 @@ class Category(Base):
 
     id = Column(INTEGER(10), primary_key=True)
     categories_fr = Column(String(250), nullable=False)
+
+    def __init__(self, id, categories_fr):
+        self.id = id
+        self.categories_fr = categories_fr
 
 
 class Product(Base):
@@ -42,6 +45,19 @@ class Product(Base):
     def __repr__(self):
         return "<Product(code='%s', url='%s', brands='%s',product_name='%s', category_id='%s', product_quantity='%s', nutrition_grade_fr='%s', stores='%s')>" % (self.code, self.url, self.brands, self.product_name, self.category_id, self.nutrition_grade_fr, self.stores)
 
+    def __init__(self, id, code, url, brands, product_name, category_id,
+                 product_name, product_quantity, nutrition_grade_fr, stores):
+
+        self.id = id
+        self.code = code
+        self.url
+        self.brands = brands
+        self.product_name = product_name
+        self.category_id = category_id
+        self.product_quantity = product_quantity
+        self.nutrition_grade_fr = nutrition_grade_fr
+        self.stores = stores
+
 
 class ProductSave(Base):
     __tablename__ = 'product_save'
@@ -57,67 +73,7 @@ class ProductSave(Base):
     product_replace = relationship(
         'Product', primaryjoin='ProductSave.product_replace_id == Product.id')
 
-
-class Connection:
-
-    def __init__(self):
-        self.source = ct.connection_source
-        self.engine = create_engine(self.source)
-        self.Session = sessionmaker(bind=self.engine)
-        self.session = self.Session()
-
-    def get_product_columns(self, column):
-        mapper = inspect(Product)
-        if column in mapper.attrs:
-            return True
-        else:
-            return False
-        # for column in mapper.attrs:
-        #     print(column.key)
-
-    def add_categories(self):
-
-        for entry in ct.categories:
-            insertion = Category(categories_fr=entry)
-            self.session.add(insertion)
-            self.session.commit()
-
-    def add_products(self):
-        """
-        Add in table product records from API's consultation for every category
-        """
-
-        # create a mapper that references Product()'s Columns
-        mapper = inspect(Product)
-        apig = api.Api_consult()
-
-        # Loop on records in Category()
-        for instance in self.session.query(Category).order_by(Category.id):
-            response = apig.api_get_results(instance.categories_fr, 1)
-
-            # Loop on pages
-            for page in range(1, apig.pages):
-
-                if page != 1:
-                    response = apig.api_get_results(
-                        instance.categories_fr, page)
-
-                # Loop on records
-                for record in response["products"]:
-
-                    # Using Dictionnary comprehension for
-                    # keeping only interresting datas
-                    record = {k: v for (k, v) in record.items()
-                              if k in mapper.attrs and k != "id"
-                              and v is not None}
-
-                    record["category_id"] = instance.id
-
-                    # Using Dictionnary unpacking
-                    # for initializing insertion argument
-                    insertion = Product(**record)
-                    print(record)
-                    print("--------------------------------------")
-                    # processing session add and commit.
-                    self.session.add(insertion)
-                    self.session.commit()
+    def __init__(self, date, product_id, product_replace_id):
+        self.date = date
+        self.product_id = product_id
+        self.product_replace_id = product_replace_id
