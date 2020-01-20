7634eb20c5980cb6a8e49492900452edb26e4479
SLB974 <slb974run@gmail.com>
2020-01-20 10:08:42 +0400

    env.sample.py modification

--- env.sample.py
+++ env.sample.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-login = "fill this information (ex : root)"
-password = "fill this information (ex : mypassword)"
-host = "fill this information (ex : localhost)"
+login = ""  # fill this information (ex : root)
+password = ""  # fill this information (ex : mypassword)
+host = ""  # fill this information (ex : localhost)
 dbase = "off_db"
 quantity_of_records_to_fetch = 15  # fill this information (ex : 15)
